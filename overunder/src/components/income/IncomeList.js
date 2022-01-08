9e6d27ca75dcde8feab0a342910aac5f609cc7d9
Parker <panoblesse@gmail.com>
2022-01-08 13:46:12 -0800

    updated form styling

--- overunder/src/components/income/IncomeList.js
+++ overunder/src/components/income/IncomeList.js
@@ -1,27 +1,31 @@
 import IncomeItem from "./IncomeItem";
 import incomeData from "../../mock-data/mock-income";
 import { Button, CloseButton, Stack } from "react-bootstrap";
+import NewIncomeForm from "./NewIncomeForm";
 
 const IncomeList = () => {
     const income = incomeData.income;
     return (
-        <div className="text-center">
-            {
-                income.length > 0 ?
-                    income.map((income) => {
-                        return (
-                            <IncomeItem
-                                key={income.id}
-                                name={income.name}
-                                amount={income.amount}
-                                frequency={income.frequency}
-                            />
-                        )
-                    }) :
-                    "No income to display."
-            }
-            <Button variant="outline-primary">Add New</Button>
-        </div>
+        <>
+            <div className="text-center">
+                {
+                    income.length > 0 ?
+                        income.map((income) => {
+                            return (
+                                <IncomeItem
+                                    key={income.id}
+                                    name={income.name}
+                                    amount={income.amount}
+                                    frequency={income.frequency}
+                                />
+                            )
+                        }) :
+                        "No income to display."
+                }
+                <Button variant="outline-primary">Add New</Button>
+            </div>
+            <NewIncomeForm />
+        </>
     );
 };
 
--- overunder/src/components/income/NewIncomeForm.js
+++ overunder/src/components/income/NewIncomeForm.js
@@ -1,30 +1,40 @@
-import { Button, Form, FormGroup } from "react-bootstrap";
+import { Button, Row, Col, Form, FormGroup } from "react-bootstrap";
 
 
 const NewIncomeForm = () => {
     return (
         <>
-            <Form>
-                <FormGroup controlId="formIncomeName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control />
-                </FormGroup>
-                <FormGroup controlId="formIncomeAmount">
-                    <Form.Label>Amount</Form.Label>
-                    <Form.Control />
-                </FormGroup>
-                <FormGroup controlId="formIncomeFrequency">
-                    <Form.Label>Frequency</Form.Label>
-                    <Form.Select aria-label="select occurrence frequency">
-                        <option value="daily">Daily</option>
-                        <option value="weekly">Weekly</option>
-                        <option value="biweekly">Bi-weekly</option>
-                        <option value="monthly">Monthly</option>
-                        <option value="quarterly">Quarterly</option>
-                        <option value="yearly">Yearly</option>
-                    </Form.Select>
-                </FormGroup>
+            <Form className="mt-2">
+                <Row>
+                    <Col>
+                        <FormGroup controlId="formIncomeName">
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup controlId="formIncomeAmount">
+                            <Form.Label>Amount</Form.Label>
+                            <Form.Control />
+                        </FormGroup>
+                    </Col>
+                    <Col>
+                        <FormGroup controlId="formIncomeFrequency">
+                            <Form.Label>Frequency</Form.Label>
+                            <Form.Select aria-label="select occurrence frequency">
+                                <option value="daily">Daily</option>
+                                <option value="weekly">Weekly</option>
+                                <option value="biweekly">Bi-weekly</option>
+                                <option value="monthly">Monthly</option>
+                                <option value="quarterly">Quarterly</option>
+                                <option value="yearly">Yearly</option>
+                            </Form.Select>
+                        </FormGroup>
+                    </Col>
+                </Row>
+                <div className="w-auto text-center mt-2">
                 <Button variant="primary" type="submit">Add</Button>
+                </div>
             </Form>
         </>
     );
