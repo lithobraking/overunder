82836409491a568bad7bf11135044c4c795784f9
lithobraking <34843669+lithobraking@users.noreply.github.com>
2022-01-22 21:51:43 +0000

    moved Income heading back inside card

--- client/src/components/Cashflow.js
+++ client/src/components/Cashflow.js
@@ -4,13 +4,15 @@ import IncomeList from "./income/IncomeList";
 const Cashflow = () => {
     return (
         <>
-            <h2><b>Income</b></h2>
             <Card className="mb-3 p-2">
                 <Container>
+                    <div className="mb-3">
+                        <h2><b>Income</b></h2>
+                    </div>
                     <div className="d-flex flex-row justify-content-around px-3">
-                        <div style={{textAlign: "center"}} className="w-100"><h5><b>Name</b></h5></div>
-                        <div style={{textAlign: "center"}} className="w-100"><h5><b>Amount</b></h5></div>
-                        <div style={{textAlign: "center"}} className="w-100"><h5><b>Frequency</b></h5></div>
+                        <div style={{ textAlign: "center" }} className="w-100"><h5><b>Name</b></h5></div>
+                        <div style={{ textAlign: "center" }} className="w-100"><h5><b>Amount</b></h5></div>
+                        <div style={{ textAlign: "center" }} className="w-100"><h5><b>Frequency</b></h5></div>
                     </div>
                     <IncomeList />
                 </Container>
