950df911764a1121f556a3d1b5a495b364b7f487
lithobraking <34843669+lithobraking@users.noreply.github.com>
2022-01-22 20:39:00 +0000

    'Amount' input field only accepts numbers

--- client/src/components/income/NewIncomeForm.js
+++ client/src/components/income/NewIncomeForm.js
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import { Button, Row, Col, Form, FormGroup } from "react-bootstrap";
+import { Button, Row, Col, Form, FormGroup, InputGroup } from "react-bootstrap";
 
 
 const NewIncomeForm = ({ open, setOpen, setIncome }) => {
     const [name, setName] = useState('');
-    const [amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState('');
     const [frequency, setFrequency] = useState('daily');
-    const [validated, setValidated] = useState(false);
+
+    const handleAmountChange = (e) => {
+        // makes sure 'Amount' input box only accepts numbers
+        const re = /^[0-9\b]+$/;
+        if (e.target.value === '' || re.test(e.target.value)) {
+            setAmount(e.target.value);
+        };
+    };
 
     const handleSubmit = (e) => {
         const form = e.currentTarget;
@@ -16,7 +23,6 @@ const NewIncomeForm = ({ open, setOpen, setIncome }) => {
         };
 
         e.preventDefault();
-        setValidated(true);
         const income = {
             id: 0,
             name: name,
@@ -43,40 +49,35 @@ const NewIncomeForm = ({ open, setOpen, setIncome }) => {
 
     return (
         <>
-            <Form
-                
-                validated={validated}
-                className="mt-2"
-                onSubmit={handleSubmit}>
+            <Form className="mt-2" onSubmit={handleSubmit}>
                 <Row>
                     <Col>
                         <FormGroup controlId="formIncomeName">
                             <Form.Label>Name</Form.Label>
                             <Form.Control
                                 type="text"
+                                placeholder='ex. "Paycheck 1"'
                                 value={name}
                                 onChange={(e) => setName(e.target.value)}
                                 required
                             />
-                            <Form.Control.Feedback type="invalid">
-                                Field cannot be blank.
-                            </Form.Control.Feedback>
                         </FormGroup>
                     </Col>
                     <Col>
                         <FormGroup controlId="formIncomeAmount">
                             <Form.Label>Amount</Form.Label>
-                            <Form.Control
-                                type="text"
-                                inputMode="numeric"
-                                value={amount}
-                                onChange={(e) => setAmount(e.target.value)}
-                                min={0}
-                                required
-                            />
-                            <Form.Control.Feedback type="invalid">
-                                Amount must be greater than zero.
-                            </Form.Control.Feedback>
+                            <InputGroup>
+                                <InputGroup.Text>$</InputGroup.Text>
+                                <Form.Control
+                                    type="text"
+                                    placeholder='ex. "1312"'
+                                    inputMode="numeric"
+                                    value={amount}
+                                    onChange={handleAmountChange}
+                                    min={0}
+                                    required
+                                />
+                            </InputGroup>
                         </FormGroup>
                     </Col>
                     <Col>
