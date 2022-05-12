c5ef771649031036d2ca564f06cbb1138faeb96f
lithobraking <34843669+lithobraking@users.noreply.github.com>
2022-05-12 22:33:14 +0000

    added navbar to page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import TotalsDisplay from "./components/TotalsDisplay";
 import Cashflow from "./components/Cashflow";
-import {Container, Row, Col} from "react-bootstrap";
+import { Container, Row, Col, Navbar } from "react-bootstrap";
 
 function App() {
 	return (
-		<Container className="vh-100 pt-5">
-			<Row>
-				<Col><Cashflow /></Col>
-				<Col><TotalsDisplay /></Col>
-			</Row>
-		</Container>
+		<>
+			<Navbar>
+				<Container className="d-flex justify-content-center">
+					<Navbar.Brand className="me-0"><h1><strong>OverUnder</strong></h1></Navbar.Brand>
+				</Container>
+			</Navbar>
+			<Container className="vh-100 pt-5">
+				<Row>
+					<Col><Cashflow /></Col>
+					<Col><TotalsDisplay /></Col>
+				</Row>
+			</Container>
+		</>
 	);
 }
 
